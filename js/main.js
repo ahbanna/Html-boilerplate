527e9fc414d15cd5b723676938d06ce5e2539ce2
Banna <tutul5800@gmail.com>
2022-11-25 18:30:38 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    "use strict";
+
+
+
+    //wow
+    new WOW().init();
+
+
+    //pop-up video
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+        // other options
+    });
+
+
+})(jQuery);
